b6024729b6297fc5739f15b1842f1e722a15fbac
Vasanth Kesavan <vk432@njit.edu>
2019-01-12 21:08:28 -0500

    seed init hello world

---
+++ lib/app_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'my-app',
+  styleUrls: ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: [],
+)
+class AppComponent {
+}
